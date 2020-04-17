1110c6805cf18897ec64d69e584115f6ff14265d
Daniel Díaz <daniel.diaz.astudillo@gmail.com>
2020-04-17 13:51:34 -0400

    Starting migration to Razor Pages

--- CoreMailerExample/Startup.cs
+++ CoreMailerExample/Startup.cs
@@ -15,6 +15,7 @@ namespace CoreMailerExample
             services.AddScoped<ICoreMvcMailer, CoreMvcMailer>();
             services.AddScoped<ITemplateRenderer, TemplateRenderer>();
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +32,7 @@ namespace CoreMailerExample
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
